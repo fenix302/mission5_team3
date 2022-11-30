147c16123ddf216255480089a15b8bf46ef67ea9
KiimMiinJii <mkim93810@gmail.com>
2022-12-01 00:43:51 +0900

    KMJ3

--- javascript/MJ/index.js
+++ javascript/MJ/index.js
@@ -1,15 +1,6 @@
-$('document').ready(function() {
-	var options = { videoId: '9cn1d-nfewI', start: 3 };
-	$('#banner').tubular(options);
-	// f-UGhWj1xww cool sepia hd
-	// 49SKbS7Xwf4 beautiful barn sepia
-});
-
 $(document).ready(function () {
-	//tublur
-	$('document').ready(function () {
-	   var options = {videoId: 'uWlCbRhYT7Y', mute: true};
-	   $('header, #main_visual, section').tubular(options);
-	});
- });
-
+   $('document').ready(function () {
+      var options = { videoId: 'FBtWixtj4NQ', mute: true };
+      $('header, #main_visual, section, #content, .con, footer, .in3').tubular(options);
+   });
+});
