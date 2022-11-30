a5524bd4714c9f662f04c3bf1a7b2439224e67b3
KiimMiinJii <mkim93810@gmail.com>
2022-11-30 19:42:47 +0900

    KMJ2

---
+++ javascript/MJ/index.js
@@ -0,0 +1,15 @@
+$('document').ready(function() {
+	var options = { videoId: '9cn1d-nfewI', start: 3 };
+	$('#banner').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
+
+$(document).ready(function () {
+	//tublur
+	$('document').ready(function () {
+	   var options = {videoId: 'uWlCbRhYT7Y', mute: true};
+	   $('header, #main_visual, section').tubular(options);
+	});
+ });
+
